128726bb463d659581d17be7efa2b1d9b5070e54
hdhdhdheu <desmondnickl+github1@gmail.com>
2024-11-13 00:59:58 -0600

    challenge 2 complete: return what leter sum is the most common and how many words have said sum

--- src/letterValueSum.java
+++ src/letterValueSum.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
 
 public class letterValueSum {
 
@@ -8,26 +9,45 @@ public class letterValueSum {
 
             BufferedReader br = new BufferedReader(new FileReader("enable1.txt"));
             String line;
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+
             while ((line = br.readLine()) != null) {
-                letterSum(line);
+                map.compute(letterSum(line), (key, value) -> (value == null) ? 1 : value + 1);
             }
             br.close();
+
+
+            System.out.println("Num: " + maxKey(map) + " Occurances: " + map.get(maxKey(map)));
 
         } catch (Exception e) {
             System.out.println("ERROR");
             e.printStackTrace();
         }
+
     }
 
-    public static void letterSum(String str) {
+    public static Integer letterSum(String str) {
         int ans = 0;
 
         for (int i = 0; i < str.length(); i++) {
             ans += (str.charAt(i) - 'a' + 1);
         }
 
-        if (ans == 319) {
-            System.out.println(str);
+        return ans;
+    }
+
+    public static Integer maxKey (HashMap<Integer, Integer> map) {
+        int maxKey = 0;
+        int maxValue = 0;
+
+        for (int i : map.keySet()) {
+            if (map.get(i) > maxValue) {
+                maxKey = i;
+                maxValue = map.get(i);
+            }
         }
+
+        return maxKey;
     }
 }
